4229b846a1c52b718628142b7f8cfbbff2644270
Nate Koch <nkoch@uoregon.edu>
2021-11-11 22:57:56 -0800

    integrated tailwind css

--- Dockerfile
+++ Dockerfile
@@ -9,12 +9,17 @@
 
 # Init the containter and update it
 FROM python:3.8 AS dev
-RUN apt -y update && apt -y upgrade
+FROM node:latest
+RUN apt -y update && apt -y upgrade && apt install npm -y && apt clean
 
 # Install necessary libraries
+COPY package*.json /app
 COPY requirements.txt /app/requirements.txt
 WORKDIR /app
 RUN pip install -r requirements.txt
+RUN npm install
+RUN npm ci --only=production
+RUN npm run build
 
 # Run app
 CMD ["python3", "server.py"]
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+//console.log(process.env.NODE_ENV);
+//const purge = process.env.NODE_ENV === 'production' ? true : false;
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ['./templates/**/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      jade: '#01A66F',
+      medium_aquamarine: "#75CE9F",
+      madang: "#BDD99E",
+      chardonnay: "#FFC06E"
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
