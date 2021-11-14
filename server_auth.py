f410edb92bf983fa6547a44331ef67b50ab6bd76
VladUO <81829347+VladUO@users.noreply.github.com>
2021-11-14 15:57:10 -0800

    Changed things to the original spec for login.
    Working on the templates.

--- server_auth.py
+++ server_auth.py
@@ -11,51 +11,34 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSigna
 from passlib.apps import custom_app_context as pwd_context
 from passlib.hash import sha256_crypt as pwd_context
 
-from pymongo import MongoClient
+from flask_login import (LoginManager, current_user, login_required,
+                         login_user, logout_user, UserMixin,
+                         confirm_login, fresh_login_required)
+from flask_wtf import FlaskForm as Form
+from wtforms import BooleanField, StringField, validators
 
-# importing our database 
-# client = MongoClient('mongodb://' + os.environ['MONGODB_HOSTNAME'], 27017)
-client = MongoClient('mongodb://ourdb', 27017)
-db = client.database
-user = client.users
+   
+class LoginForm(Form):
+    username = StringField('Username', [
+        validators.Length(min=2, max=25,
+        message=u"A little too short or long for a username."),
+        validators.InputRequired(u"Input is required.")])
+    password = StringField('Password', [
+        validators.Length(min=2, max=25,
+        message=u"Huh, little too short for a password."),
+        validators.InputRequired(u"Forget something?")])    
+    remember = BooleanField('Remember me')    
 
-SECRET_KEY = "im super secret"
+class RegisterForm(Form):
+    username = StringField('Username', [
+        validators.Length(min=2, max=25,
+        message=u"A little too short or long for a username."),
+        validators.InputRequired(u"Input is required.")])
+    password = StringField('Password', [
+        validators.Length(min=2, max=25,
+        message=u"Huh, little too short for a password."),
+        validators.InputRequired(u"Forget something?")])   
 
-
-class register(Resource): #has to be a POST
-    def post(self):
-        # hash the password, insert usename and hashed pword into the database
-        app.logger.debug("ENTERED USER REGISTRATION")
-
-        username = request.form.get('username', default = "Default", type=str)
-        password = pwd_context.encrypt(request.form.get('password', default = "None", type=str))
-       
-        findUser = user.users.find_one({"username": username})
-        if (findUser):
-            return {"message": "<h1>A user with that name already exists, please pick another user name.</h1>"}, 400
-        else:
-            user.users.insert({"username": username, "password": password})
-            return {"message": "<H1>Registration success!</h1>"}, 201
-        
-
-class token(Resource):
-    def get(self):
-        # hash the password, compare to database, seturn error if not in there, token otherwise
-        app.logger.debug("ENTERED USER LOGIN")
-        
-        username = request.args.get('username', default = "None", type=str)
-        password = request.args.get('password', default = "None", type=str)
-       
-        findUser = user.users.find_one({"username": username})
-        if (not findUser):
-            return {"message": "No user matching that user name found!"}, 401
-        else:
-            if (verify_password(password, findUser["password"])):
-                token = generate_auth_token(username).decode('utf-8')
-               
-                return {"message": "success", "token":token, "valid":10, "id": str(findUser["_id"])}, 200  
-            else:
-                return {"message": "Password does not match the one on record!"}, 401
 
 def hash_password(password):
     return pwd_context.encrypt(password)
@@ -64,21 +47,6 @@ def hash_password(password):
 def verify_password(password, hashVal):
     return pwd_context.verify(password, hashVal)
 
-def generate_auth_token(username, expiration=10):
-    s = Serializer(SECRET_KEY, expires_in=expiration)
-    return s.dumps({"username": username})
-
-
-def verify_auth_token(token):
-    s = Serializer(SECRET_KEY)
-    try:
-        data = s.loads(token)
-    except SignatureExpired:
-        return False, "Expired token!"    # valid token, but expired
-    except BadSignature:
-        return False, "Invalid token!"    # invalid token
-    return True, "success"
-
 
 # The blueprint for Flask to load in the main server file
 blueprint = flask.Blueprint("auth_blueprint", __name__)
@@ -86,39 +54,36 @@ blueprint = flask.Blueprint("auth_blueprint", __name__)
 # The Flask login manager
 login_manager = flask_login.LoginManager()
 
-def is_admin():
+@login_required
+def is_admin(username):
     """
     Returns whether the user is an administrator
     """
-    return False # TODO
+    #get the list of current admins
+    admins = flask.current_app.db.fetch_admins(user_name = username)
+    if username in admins:
+        return True
+    return False
 
-def is_authenticated():
+def is_authenticated(username):
     """
     Return whether the user is logged in
     """
-    return False  # TODO
+    if username in session:
+        print('You are loggen in as ' + session['username'])
+        return True 
+    return False  
 
 @login_manager.user_loader
 def user_loader(username):
     """
     Loads the user if it exists
     """
-    user = flask.current_app.db.fetch_user(userid=None, user_name=username)
-
-    pass # TODO
-
-# Page endpoints
-
-class User(flask_login.UserMixin):
-    """
-    The user class object for session/auth purposes
-    """
+    user = flask.current_app.db.fetch_user(user_name=username)
+    if user.get('username') == username:
+        session['username'] = username        
+    return 'The user with username' + username + ' has been loaded in.'
     
-    def __init__(self):
-        pass # TODO
-
-    def get_id(self):
-        return str(self.id)
 
 @blueprint.route("/register.html", methods=["GET", "POST"])
 def register():
@@ -126,7 +91,27 @@ def register():
     GET: Returns the register account page register.html
     POST: Attempts to register the account and redirects to the login page login.html
     """
-    pass # TODO
+    form = RegisterForm()
+    #handling the POST requests
+    if request.method == "POST": 
+        if form.validate_on_submit() and "username" in request.form and "password" in request.form:
+            username = request.form["username"].encode('utf-8')
+            password = request.form["password"].encode('utf-8')
+                
+                    
+            #search for a user with the provided user name
+            user = flask.current_app.db.fetch_user(user_name = username)
+            #if the user isnt found, hash the password and register the new user      
+            if user.get('username') is None:
+                hashed_pass = hash_password(password)
+                flask.current_app.db.add_user(user_name = username, password = hashed_pass)
+                session['username'] = request.form['username']
+                return redirect(url_for('home.html'))
+            else:
+                return 'A user with that username already exists, please try another username.'   
+    #otherwise it is a GET request or error and we render the register page
+    else:
+        return render_template("register.html", form=form)
 
 @blueprint.route("/login.html", methods=["GET", "POST"])
 def login():
@@ -134,15 +119,40 @@ def login():
     GET: Returns the login page login.html
     POST: Attempts to log in the user and redirects to my boards myboards.html
     """
-    pass # TODO
+    form = LoginForm()
+    #handling the POST request
+    if request.method == "POST":
+        #checking for valid username and password field
+        if form.validate_on_submit() and "username" in request.form and "password" in request.form:
+            username = request.form["username"].encode('utf-8')
+            password = request.form["password"].encode('utf-8')
+            
+            user = flask.current_app.db.fetch_user(user_name = username)
+            #if that username exits, checking the password for a match
+            if user.get('username') is not None:
+                if user.get('password') == verify_password(password, user.get('password')):
+                    session['username'] = username        
+                    return redirect(url_for('myboards.html'))
+                else:
+                    return 'Invalid username or password, please try again!'
+            else:
+                return 'Invalid username or password, please try again!'    
+    #handling invalid inputs and the GET requests        
+        flash('Invalid input, please try again!')
+        return render_template("login.html", form=form)
+    else:
+        return render_template("login.html", form=form)
 
 @blueprint.route("/logout")
+@login_required
 def logout():
     """
     Logs out the user and redirects to the home page home.html
     """
-    pass # TODO
+    logout_user()
+    flash("Logged out.")
+    return redirect(url_for("home.html"))
 
-# Run the application
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+# # Run the application
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True)
