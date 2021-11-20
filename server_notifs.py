82ed9efde5b20edd94ae600b7883c2822b21e1f0
Andy Nguyen <nguyenandy41@gmail.com>
2021-11-19 23:11:24 -0800

    Integrate push notifications with database

--- server_notifs.py
+++ server_notifs.py
@@ -11,14 +11,14 @@ import pywebpush
 import json
 import threading
 
+from bson import ObjectId
+
 import config
 import db_connect
+import server_auth
 
 # The blueprint for Flask to load in the main server file
 blueprint = flask.Blueprint("notifs_blueprint", __name__)
-
-# Temporary storage for subscriptions TODO move this into database
-temp_subscriptions = []
 
 @blueprint.route("/push/subscription", methods=["GET", "POST"])
 def push_subscription():
@@ -39,20 +39,19 @@ def push_subscription():
             content_type="application/json"
         )
     else:
+        # User must be subscribed to do notifications
+        if not server_auth.is_authenticated():
+            return flask.jsonify({"error": "You must be logged in to subscribe to notifications"}), 403
+
         # Add or remove subscription from database
-        global temp_subscriptions
         subscription_token = flask.request.get_json().get("subscription_token", "")
         if subscription_token is None or subscription_token == "":
             return flask.jsonify({"error": "Invalid subscription"}), 400
         add_sub = flask.request.get_json().get("subscribe", "False")
-        if add_sub == True:
-            # For now add to local array. TODO add to database
-            temp_subscriptions.append(subscription_token)
+        if add_sub:
+            db_connect.get_db().add_notification(userid=None, user_name=server_auth.get_curr_username(), notification=subscription_token)
         else:
-            # For now remove from local array. TODO remove from database
-            p256dh = subscription_token["keys"]["p256dh"] # Identifier for key
-            temp_subscriptions = [x for x in temp_subscriptions if x["keys"]["p256dh"] != p256dh]
-        #print("Subscriptions list:", temp_subscriptions)
+            db_connect.get_db().remove_notification(userid=None, user_name=server_auth.get_curr_username(), notification=subscription_token)
         return flask.Response(status=200, mimetype="application/json")
 
 @blueprint.route("/push/test", methods=["POST"])
@@ -61,16 +60,15 @@ def push_test():
     Test push notifications via web push. TODO remove or protect in production
     """
     # Validate message
-    message = flask.request.get_json().get("message", "")
-    if message is None or message == "":
-        return flask.jsonify({"error": "Bad message"}), 400
-    return do_push_notifications(board_id=message, post_id=None)
+    post_id = flask.request.get_json().get("post_id", "")
+    board_id = flask.request.get_json().get("board_id", "")
+    if post_id is None or post_id == "" or board_id is None or board_id == "":
+        return flask.jsonify({"error": "Bad payload"}), 400
+    return do_push_notifications(board_id=ObjectId(board_id), post_id=ObjectId(post_id))
 
-def do_push_notifications(board_id, post_id):
+def do_push_notifications(board_id: ObjectId, post_id: ObjectId):
     """
-    Performs the action of notifying all users of a post. This is an expensive operation
-
-    If testing, set post_id to None and board_id to the message to send
+    Performs the action of notifying all users of a post. This is an expensive operation but will return immediately
 
     Parameters:
      - board_id: The board ID of the post
@@ -88,30 +86,19 @@ def do_push_notifications(board_id, post_id):
         return flask.jsonify({"error": "Server does not have a valid VAPID private key"}), 503
 
     # Define function to run in worker thread (expensive)
+    db_obj = db_connect.get_db()
     def _push_notif_worker(board_id, post_id, vapid_email, private_key):
-        post = db_connect.get_db().fetch_post(post_id)
-        msg = post["post_subject"]
-        board_members = db_connect.get_db().fetch_board(board_id)["board_members"]
+        post = db_obj.fetch_post(board_id, post_id)
+        msg = post.get("post_subject", "Unknown post subject")
+        board_members = db_obj.fetch_board(board_id).get("board_members", [])
         for m in board_members:
-            member = db_connect.get_db().fetch_user(userid=m, user_name=None)
-            subscriptions = [] # TODO get user subscriptions here
+            member = db_obj.fetch_user(userid=m, user_name=None)
+            subscriptions = member.get("notification", [])
             for s in subscriptions:
                 send_web_push(s, msg, vapid_email, private_key)
 
-    # Define test function to run in worker thread (expensive)
-    def _push_notif_worker_test(msg, vapid_email, private_key):
-        global temp_subscriptions
-        for sub in temp_subscriptions:
-            send_web_push(sub, msg, vapid_email, private_key)
-
     # Setup thread (daemon) worker and start it
-    if post_id is None:
-        target = _push_notif_worker_test
-        args = (board_id, vapid_email, private_key,)
-    else:
-        target = _push_notif_worker
-        args = (board_id, post_id, vapid_email, private_key,)
-    t = threading.Thread(target=target, args=args, daemon=True)
+    t = threading.Thread(target=_push_notif_worker, args=(board_id, post_id, vapid_email, private_key,), daemon=True)
     t.start()
 
     # Finally, send back a proper response
