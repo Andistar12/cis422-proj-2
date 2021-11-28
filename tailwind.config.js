a55332ce706015c75aea398fee5c49e0e9f2c732
Nate Koch <31897172+natekoch@users.noreply.github.com>
2021-11-28 14:29:09 -0800

    purge unused tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 //const purge = process.env.NODE_ENV === 'production' ? true : false;
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ['./templates/**/*.html']
   },
   darkMode: false, // or 'media' or 'class'
