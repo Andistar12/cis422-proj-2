08553029989b005a3c97858e069079653be59e69
Andistar12 <nguyenandy41@gmail.com>
2021-11-29 13:30:21 -0500

    Integrate makepost, add button to go back

--- static/js/makepost.js
+++ static/js/makepost.js
@@ -1,6 +1,22 @@
 $(document).ready(function () {
+
+    let board_id = new URLSearchParams(window.location.search).get("board");
+
+    // Update button to link back to correct board
+    document.getElementById("board-link").href = "/viewboard.html?board=" + board_id;
+
+
+    let succ = function(data) {
+        // Update board name field, then make page visible
+        document.getElementById("board_name").innerHTML = data["board_name"]; 
+        document.getElementById("makepost_content").hidden = false;
+    }
+    let error = function(err) {
+        console.log(get_error(err));
+    }
+    fetch_board(board_id, succ, error);
+
     document.getElementById("makepost_submit").addEventListener("click", function () {
-        let board_id = new URLSearchParams(window.location.search).get("board");
         let sub = document.getElementById("makepost_subject").value;
         let desc = document.getElementById("makepost_desc").value;
 
